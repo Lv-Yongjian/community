0150f4f61c2431b1fddc3ec7048f064f1b13a543
Lv-Yongjian <1071678079@qq.com>
2020-01-16 09:57:00 +0800

    extract github config from code to properties

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-##社区论坛  
+## 社区论坛  
 
 
-##资料  
-https://spring.io/  
-https://spring.io/guides/gs/serving-web-content/  
-https://elasticsearch.cn/  
-https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/  
+## 资料  
+[Spring 文档](https://spring.io/)  
+[Spring Web](https://spring.io/guides/gs/serving-web-content/)  
+[es](https://elasticsearch.cn/)  
+[Github deploy key](https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys)
+[Bootstrap](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)  
+[Github OAuth](https://developer.github.com/apps/building-github-apps/creating-a-github-app/)
 
-##工具  
-https://git-scm.com/  
+## 工具  
+[Git](https://git-scm.com/)  
+[Visual Paradigm](https://www.visual-paradigm.com)
---
+++ src/main/java/com/hciot/community/controller/AuthorizeController.java
@@ -0,0 +1,41 @@
+package com.hciot.community.controller;
+
+import com.hciot.community.dto.AccesstokenDTO;
+import com.hciot.community.dto.GithubUser;
+import com.hciot.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect_uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccesstokenDTO accesstokenDTO = new AccesstokenDTO();
+        accesstokenDTO.setClient_id(clientId);
+        accesstokenDTO.setClient_secret(clientSecret);
+        accesstokenDTO.setCode(code);
+        accesstokenDTO.setRedirect_uri(redirectUri);
+        accesstokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accesstokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
