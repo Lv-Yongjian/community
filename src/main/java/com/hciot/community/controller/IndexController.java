55ff7049a7cf02251d79193bc8457f60e70a18cf
Lv-Yongjian <1071678079@qq.com>
2020-01-16 17:19:37 +0800

    add check cookies

--- src/main/java/com/hciot/community/controller/IndexController.java
+++ src/main/java/com/hciot/community/controller/IndexController.java
@@ -1,13 +1,36 @@
 package com.hciot.community.controller;
 
+import com.hciot.community.mapper.UserMapper;
+import com.hciot.community.model.User;
+import jdk.nashorn.internal.parser.Token;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+    @Autowired(required = false)
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String hello() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
         return "index";
     }
 }
